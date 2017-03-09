d51fe84b5c00500c4f0b8f52ffb72794f3ee8faf
Lukasz Chmielewski <lu.chmielewski@gmail.com>
2017-03-09 18:43:56 +0100

    Fixed the path reference in controllers

--- controller/player_controller.js
+++ controller/player_controller.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var router = express.Router();
-var Player = require('./models/Player');
+var Player = require.main.require('./models/Player');
 
 router.use(bodyParser.json()); // Accept incoming JSON entities
 
