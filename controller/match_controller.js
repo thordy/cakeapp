32684b39827e9976ee2532d712ba7b7618c9bd63
Lukasz Chmielewski <lu.chmielewski@gmail.com>
2017-03-12 15:43:00 +0100

    Changed the way of fetching players for the match

--- controller/match_controller.js
+++ controller/match_controller.js
@@ -7,93 +7,91 @@ var Match = require.main.require('./models/Match');
 var helper = require('../helpers.js');
 
 router.use(bodyParser.json()); // Accept incoming JSON entities
-router.use(bodyParser.urlencoded( {extended: true} ));
+router.use(bodyParser.urlencoded({extended: true}));
 
 
 /* Get a list of all matches */
 router.get('/list', function (req, res) {
-	Match.find(function(err, matches) {
-		if (err) {
-			return helper.renderError(res, err);
-		}
-		res.render('matches', {matches: matches});
-	});
+    Match.find(function (err, matches) {
+        if (err) {
+            return helper.renderError(res, err);
+        }
+        res.render('matches', {matches: matches});
+    });
 });
 
 /* Render the match view */
 router.get('/:id', function (req, res) {
-	var id = req.params.id;
-	Match.findById(id, function(err, match) {
-		if (err) {
-			return helper.renderError(res, err);
-		}
-
-        Player.find({'_id' : { $in : match.players }}, function(err, players) {
-            res.render('match', {match: match, players: players});
-		});
-	});
+    var id = req.params.id;
+    Match.findById(id, function (err, match) {
+        if (err) {
+            return helper.renderError(res, err);
+        }
+    }).populate('players').exec(function (err, match) {
+        res.render('match', {match: match});
+    });
 });
 
 /* Render the results view */
 router.get('/:id/results', function (req, res) {
-	res.status(202)
-		.send('Not Yet Implemented')
-		.end();
+    res.status(202)
+        .send('Not Yet Implemented')
+        .end();
 });
 
 
 /* Method for starting a new match */
 router.post('/new', function (req, res) {
-	if (req.body.players === undefined) {
-		console.log('No players specified, unable to start match');
-		return res.redirect('/');
-	}
+    if (req.body.players === undefined) {
+        console.log('No players specified, unable to start match');
+        return res.redirect('/');
+    }
 
-	var match = new Match({
-		startingScore: req.body.startingScore,
-		out: req.body.out,
-		startTime: moment()
-	});
-	match.setPlayers(req.body.players);
+    var match = new Match({
+        startingScore: req.body.startingScore,
+        out: req.body.out,
+        startTime: moment()
+    });
+    match.setPlayers(req.body.players);
 
-	match.save(function(err) {
-		if (err) {
-			return helper.renderError(res, err);
-		}
-		res.redirect('/match/' + match.id);
-	});
+    match.save(function (err) {
+        if (err) {
+            return helper.renderError(res, err);
+        }
+        res.redirect('/match/' + match.id);
+    });
 });
 
 /* Method to register three thrown darts */
 router.put('/:id/throw/', function (req, res) {
-	var matchId = req.params.id;
-	var playerId = req.body.player_id;
-	var firstDart = req.body.first_dart;
-	var secondDart = req.body.second_dart;
-	var thirdDart = req.body.third_dart;
+    var matchId = req.params.id;
+    var playerId = req.body.player_id;
+    var firstDart = req.body.first_dart;
+    var secondDart = req.body.second_dart;
+    var thirdDart = req.body.third_dart;
 
-	res.status(202)
-		.send('Not Yet Implemented')
-		.end();
+    res.status(202)
+        .send('Not Yet Implemented')
+        .end();
 });
 
 /* Method to cancel a match in progress */
-router.delete('/:id/cancel', function(req, res) {
-	Match.remove({ _id: req.params.id }, function(err) {
-		if (err) {
-		return helper.renderError(res, err);
-	}
-	res.status(204)
-		.send()
-		.end();
-	});	
+router.delete('/:id/cancel', function (req, res) {
+    Match.remove({_id: req.params.id}, function (err) {
+        if (err) {
+            return helper.renderError(res, err);
+        }
+        res.status(204)
+            .send()
+            .end();
+    });
 });
 
 /* Method to finalize a match */
 router.post('/finish', function (req, res) {
-	res.status(202)
-		.send('Not Yet Implemented')
-		.end();
+    res.status(202)
+        .send('Not Yet Implemented')
+        .end();
 });
 
 module.exports = router
