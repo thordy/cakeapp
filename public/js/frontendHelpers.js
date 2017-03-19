ee9de9fd67b21cfe520bcfd9593bde634fe9b94e
Thord Setsaas <thord@ieee.org>
2017-03-19 13:20:35 +0100

    New frontendHelpers file which defines common resuable functions

---
+++ public/js/frontendHelpers.js
@@ -0,0 +1,55 @@
+
+/**
+ * Execute a jQuery Ajax POST
+ * @param {String} url - URL to execute against
+ * @param {Object} data - Data to send 
+ * @param {String} contentType - Content type string  
+ * @param {function} success - Function callback on success
+ * @param {function} error - Function callback on error
+ */
+function executePost(url, data, contentType, success, error) {
+	$.ajax({
+		url: url,
+		type: 'POST',
+		data: data,
+		contentType: contentType,
+		success: success,
+		error: error
+	});	
+}
+
+/**
+ * Execute a jQuery Ajax PUT
+ * @param {String} url - URL to execute against
+ * @param {Object} data - Data to send 
+ * @param {String} contentType - Content type string  
+ * @param {function} success - Function callback on success
+ * @param {function} error - Function callback on error
+ */
+function executePut(url, data, contentType, success, error) {
+	$.ajax({
+		url: url,
+		type: 'PUT',
+		data: data,
+		contentType: contentType,
+		success: success,
+		error: error
+	});
+}
+
+/**
+ * Execute a jQuery Ajax DELETE
+ * @param {String} url - URL to execute against
+ * @param {Object} data - Data to send 
+ * @param {String} contentType - Content type string  
+ * @param {function} success - Function callback on success
+ * @param {function} error - Function callback on error
+ */
+function executeDelete(url, success, error) {
+	$.ajax({
+		url: url,
+		type: 'DELETE',
+		success: success,
+		error: error
+	});	
+}
