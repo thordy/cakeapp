7204faf855f2b573be2cda1d84948553f5d5581f
Thord Setsaas <t.setsaas@sportradar.com>
2017-04-05 10:21:55 +0200

    Setup fastclick

--- app.js
+++ app.js
@@ -32,14 +32,14 @@ app.use(function(req, res, next){
 
   // respond with html page
   if (req.accepts('html')) {
-	res.render('404', { url: req.url });
-	return;
+	 res.render('404', { url: req.url });
+	 return;
   }
 
   // respond with json
   if (req.accepts('json')) {
-	res.send({ error: 'Not found' });
-	return;
+	 res.send({ error: 'Not found' });
+	 return;
   }
 
   // default to plain-text. send()
