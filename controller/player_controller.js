c3a8c3a1d1f6f07e50cfbd50b9e0d3f95efa0d73
Thord Setsaas <thord@ieee.org>
2017-04-06 20:48:53 +0200

    Implemented player comparison page

--- controller/player_controller.js
+++ controller/player_controller.js
@@ -28,43 +28,13 @@ router.get('/:id/stats', function(req, res) {
 		.fetch()
 		.then(function(row) {
 			var player = row.serialize();
-			player.ppdScore = 0;
-			player.ppd = 0;
-			player.first9ppd = 0;
-			player.first9Score = 0;
-			player.totalScore = 0;
-			player.visits = 0;
-			player.highScores = { '60+': 0, '100+': 0, '140+': 0, '180': 0 };
 
 			Score.where( {player_id: req.params.id} )
 			.fetchAll()
 			.then(function (scoreRows) {
 				var scores = scoreRows.serialize();
-				for (var i = 0; i < scores.length; i++) {
-					var score = scores[i];
-
-					var totalVisitScore = (score.first_dart * score.first_dart_multiplier) +
-							(score.second_dart * score.second_dart_multiplier) +
-							(score.third_dart * score.third_dart_multiplier);
-
-					player.visits += 1;
-					player.ppdScore += totalVisitScore;
-
-					if (totalVisitScore >= 60 && totalVisitScore <= 99) {
-						player.highScores['60+'] += 1;
-					}
-					else if (totalVisitScore >= 100 && totalVisitScore <= 139) {
-						player.highScores['100+'] += 1;
-					}
-					else if (totalVisitScore >= 140 && totalVisitScore <= 179) {
-						player.highScores['140+'] += 1;
-					}
-					else if (totalVisitScore == 180) {
-						player.highScores['180'] += 1;
-					}
-				}
-				player.ppd = player.ppdScore / (player.visits * 3);
-				res.render('playerStatistics', {player: player} );
+				var statistics = getPlayerStatistics([player], scores)[player.id];
+				res.render('playerStatistics', { player: statistics } );
 			});
 		})
 		.catch(function(err) {
@@ -79,11 +49,81 @@ router.get('/list', function (req, res) {
 		})
 		.fetchAll()
 		.then(function(players) {
-			res.render('players', {players: players.serialize()});
+			res.render('players', { players: players.serialize() });
 		})
 		.catch(function(err) {
 			helper.renderError(res, err);
 		});
 });
+
+/* Get comparable statistics for the given players s*/
+router.get('/compare', function (req, res) {
+	var playerIds = req.query.player_id;
+	debug('Comparing players %s', playerIds);
+	Player
+		.where('id', 'IN', playerIds)
+		.fetchAll()
+		.then(function(rows) {
+			var players = rows.serialize();
+			Score
+				.where('player_id', 'IN', playerIds)
+				.fetchAll()
+				.then(function(scoreRows){
+					var playerMap = getPlayerStatistics(players, scoreRows.serialize());
+					res.render('playerComparison', { players: playerMap });
+				});
+		});
+});
+
+function getPlayerStatistics(players, scores) {
+	var playerMap = {};
+	for (var i = 0; i < players.length; i++) {
+		var player = players[i];
+		playerMap[player.id] = {
+			id: player.id,
+			name: player.name,
+			gamesWon: player.games_won,
+			gamesPlayed: player.games_played,
+			ppdScore: 0,
+			ppd: 0,
+			first9ppd: 0,
+			first9Score: 0,
+			totalScore: 0,
+			visits: 0,
+			scores: [],
+			highScores: { '60+': 0, '100+': 0, '140+': 0, '180': 0 }
+		}
+	}
+
+	for (var i = 0; i < scores.length; i++) {
+		var score = scores[i];
+		var player = playerMap[score.player_id];
+
+		var totalVisitScore = (score.first_dart * score.first_dart_multiplier) +
+				(score.second_dart * score.second_dart_multiplier) +
+				(score.third_dart * score.third_dart_multiplier);
+
+		player.visits += 1;
+		player.ppdScore += totalVisitScore;
+
+		if (totalVisitScore >= 60 && totalVisitScore <= 99) {
+			player.highScores['60+'] += 1;
+		}
+		else if (totalVisitScore >= 100 && totalVisitScore <= 139) {
+			player.highScores['100+'] += 1;
+		}
+		else if (totalVisitScore >= 140 && totalVisitScore <= 179) {
+			player.highScores['140+'] += 1;
+		}
+		else if (totalVisitScore == 180) {
+			player.highScores['180'] += 1;
+		}
+	}
+	for (id in playerMap) {
+		var player = playerMap[id];
+		player.ppd = player.ppdScore / (player.visits * 3);
+	}
+	return playerMap;	
+}
 
 module.exports = router
--- public/js/frontendHelpers.js
+++ public/js/frontendHelpers.js
@@ -1,5 +1,20 @@
 
 /**
+ * Execute a jQuery Ajax Get
+ * @param {String} url - URL to execute against
+ * @param {function} success - Function callback on success
+ * @param {function} error - Function callback on error
+ */
+function executeGet(url, success, error) {
+	$.ajax({
+		url: url,
+		type: 'GET',
+		success: success,
+		error: error
+	});	
+}
+
+/**
  * Execute a jQuery Ajax POST
  * @param {String} url - URL to execute against
  * @param {Object} data - Data to send 
